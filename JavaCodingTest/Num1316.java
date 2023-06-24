fc6ddf5ed5e21defbb15a4038f22670439949112
Jo Seung Hyo <seunghyo7742@naver.com>
2023-06-24 23:30:04 +0900

    백준 Num1316- java

--- JavaCodingTest/Num1316.java
+++ JavaCodingTest/Num1316.java
@@ -3,22 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 public class Num1316 {
   public static void main(String[] args) throws IOException{
-    int result=0; //그룹숫자 카운트
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
     int n = Integer.parseInt(br.readLine()); //Int
-    for(int i=0; i<n; i++){
+    int result=n; //그룹숫자 카운트
 
+    for(int i=0; i<n; i++){
+      boolean[] check= new boolean[26]; // 알파벳이 26자니까 
       String str= br.readLine(); 
-      
-      for(int j=0; i<str.length(); j++){
-        String sub= str.substring(j+2);
-        if(sub.indexOf(str.charAt(j))!=-1 && str.charAt(j+1)!= str.charAt(j) ){
-          break;
+      check[(int)str.charAt(0)-97]=true;
+      for(int j=1; j<str.length(); j++){
+        
+        char target= str.charAt(j);
+        if(check[(int)target - 97]==false){//알파벳 'a'의 아스키코드는 97 , 만약 아직 나오지 않았던 알파벳이면
+          check[(int)target-97]=true;
+          continue;
         }
+        else{
+          if(str.charAt(j-1)!= target){
+            result--;
+            break;
+          }
+        }
+
       }
-      result++;
-      }
-      System.out.println(result);
     }
+  System.out.println(result);
   }
+}
 
