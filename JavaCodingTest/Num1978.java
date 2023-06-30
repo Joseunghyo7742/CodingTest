2ceab6b735c004cb19000828552b059c79a58c58
Jo Seung Hyo <seunghyo7742@naver.com>
2023-06-30 17:15:22 +0900

    백준-Num1978- java

---
+++ JavaCodingTest/Num1978.java
@@ -0,0 +1,29 @@
+  import java.io.*;
+
+  public class Num1978 {
+    public static void main(String [] args)throws IOException{
+      BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+      int n= Integer.parseInt(br.readLine());
+      String[] firstLine = br.readLine().split(" ");
+      int count=0; //소수 아닌 수 세기
+      for(int i=0; i<n; i++){
+        int num= Integer.parseInt(firstLine[i]);
+        if( num==1){
+          count++;
+        }
+        else{
+          for(int j=2; j<=500; j++){
+          if(num%j!=0 || num==j){
+            continue;
+          }
+          else{
+            count++;
+            break;
+          }
+          }
+          
+        }
+      }
+      System.out.print(n-count);
+    }
+  }
