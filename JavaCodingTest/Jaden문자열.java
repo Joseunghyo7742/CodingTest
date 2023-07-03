623d9ef75f31a99f51305c90e5facb30ea026461
Jo Seung Hyo <seunghyo7742@naver.com>
2023-07-03 15:32:51 +0900

    Java_lv2_ JadenCase문자열만들기

--- JavaCodingTest/Jaden문자열.java
+++ JavaCodingTest/Jaden문자열.java
@@ -2,22 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        String [] words= s.split("\\s");
-        for(String word : words){
-            char first_c= word.charAt(0);
-            String temp= word.substring(1,word.length()).toLowerCase();
-            
-            if((int)'0'<=(int)first_c && (int)first_c<=(int)'9'){
-                answer+=Character.toString(first_c);
-            }
-            else{
-                answer+=Character.toString(first_c).toUpperCase();
-            }
-            answer+=temp;
-            answer+=" ";
+        s=s.toLowerCase();
+        StringTokenizer st= new StringTokenizer(s, " ", true); 
+        // true => flag값으로 구분자도 나눠줌.
+        String result= "";
+        while(st.hasMoreTokens()){
+            String word= st.nextToken();
+            if(word.equals(" ")) result+=word;
+            else result+=word.substring(0,1).toUpperCase()+word.substring(1);
         }
-        answer=answer.trim();
-        return answer;
+        return result;
     }
 }
