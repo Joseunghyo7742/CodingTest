48af240a5d944b48f076df48d9e57ffbe2036dec
Jo Seung Hyo <seunghyo7742@naver.com>
2023-07-06 19:46:02 +0900

    다른사람풀이

--- JavaScriptCodingTest/성격유형검사하기.js
+++ JavaScriptCodingTest/성격유형검사하기.js
@@ -26,3 +26,15 @@ function solution(survey, choices) {
   }
   return answer;
 }
+
+//다른사람 풀이
+function solution(survey, choices){
+  const data={R:0, T:0, C:0, F:0, J:0, M:0, A:0, N:0};
+  for(let i=0; i, survey.length; i++){
+    const score= choices[i]-4;
+    let type = survey[i].split('')[score < 0 ? 0 : 1] 
+    data[type]+= Math.abs(score);
+  }
+  const {R,T,C,F,J,M,A,N}= data;
+  return `${R >= T ? 'R' : 'T'}${C >= F ? 'C' : 'F'}${J >= M ? 'J' : 'M'}${A >= N ? 'A' : 'N'}`
+}
