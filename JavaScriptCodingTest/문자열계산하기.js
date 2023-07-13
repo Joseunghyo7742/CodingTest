2cbd65d566fc9ce394efde970949fadc1c582689
Jo Seung Hyo <seunghyo7742@naver.com>
2023-07-13 13:48:45 +0900

    js_lv0_문자열계산하기

--- JavaScriptCodingTest/문자열계산하기.js
+++ JavaScriptCodingTest/문자열계산하기.js
@@ -1,9 +1,15 @@
 function solution(my_string) {
-  var answer = BigInt(my_string[0])
-  for(let i=1; i<my_string.length; i++){
-      if(my_string[i]==="+")
-          answer+= BigInt(my_string[i+=2])
-      if(my_string[i]==='-') answer-=BigInt(my_string[i+=2])
+  let arr= my_string.split(' ')
+  let answer= Number(arr[0])
+  for(let i=1; i<arr.length; i++)
+  {
+      if(arr[i]==='+'){
+          answer+=Number(arr[++i])
+      }
+      if(arr[i]==='-')
+          answer-=Number(arr[++i])
   }
-  return answer;
+  return answer
 }
+
+//자릿수를 고려했어야함
