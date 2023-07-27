e991f36342cde53732d2f2779b9e768eac313ebb
Jo Seung Hyo <seunghyo7742@naver.com>
2023-07-27 17:20:26 +0900

    백준_n2468.안전영역

--- PythonCodingTest/백준/단지번호붙이기.py
+++ PythonCodingTest/백준/단지번호붙이기.py
@@ -3,34 +3,34 @@ sys.setrecursionlimit(10 ** 6)
 
 n= int(input())
 arr= []
-dx=[0,0,-1,1]
+dx=[0,0,-1,1] #방향 설정
 dy=[-1,1,0,0]
-dangi= []
-size=1
+dangi= [] #단지 내 집 개수 담아줄 배열
+cnt=1 #단지 내 집 수 카운트
 
 for i in range(n):
-  arr.append(list(map(int,input())))
+  arr.append(list(map(int,input()))) 
   
 def dfs(x,y):
   global size;
-  for k in range(4):
+  for k in range(4): #동서남북 확인
     nx= x+dx[k]
     ny= y+dy[k]
-    if(nx<0 or ny<0 or nx>=n or ny>= n): continue
-    if(arr[nx][ny]==1):
-      size+=1
-      arr[nx][ny]=2 #들어간 집 체크
+    if(nx<0 or ny<0 or nx>=n or ny>= n): continue #범위를 벗어난 경우
+    if(arr[nx][ny]==1): #집이 있는 경우우
+      cnt+=1 #집 개수 카운트
+      arr[nx][ny]=2 #카운트한 집은 2로 바꿔준다
       dfs(nx,ny)
 
 for i in range(n):
   for j in range(n):
     if(arr[i][j]==1):
-      arr[i][j]=2 #방문한 것으로 체크
+      arr[i][j]=2 #카운트한 집으로 체크
       dfs(i,j)
-      dangi.append(size) #단지 내 집 수 배열에 추가
-      size=1 #사이즈 초기화
+      dangi.append(cnt) #단지 내 집 수 배열에 추가
+      cnt=1 #집 개수 초기화
       
-dangi.sort()
-print(len(dangi))
+dangi.sort() #오름차순 정렬
+print(len(dangi)) #단지 개수 출력
 for k in dangi:
   print(k)
