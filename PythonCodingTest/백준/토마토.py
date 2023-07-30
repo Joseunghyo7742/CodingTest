8db42772d04e6e9487c1c855327fdaf30b376ade
Jo Seung Hyo <seunghyo7742@naver.com>
2023-07-30 21:43:36 +0900

    백준_n7576:토마토 다른방식

--- PythonCodingTest/백준/토마토.py
+++ PythonCodingTest/백준/토마토.py
@@ -1,3 +1,38 @@
+# from collections import deque
+# m,n= map(int,input().split())
+# box=[]
+# queue= deque()
+# dx=[0,0,-1,1]
+# dy=[1,-1,0,0]
+# for _ in range (n):
+#   box.append(list(map(int,input().split())))
+
+# for i in range(n):
+#   for j in range(m):
+#     if box[i][j]==1:
+#       queue.append((i,j))
+
+
+# while queue:
+#   x,y= queue.popleft()
+#   for i in range(4):
+#     nx= x+ dx[i]
+#     ny= y+ dy[i]
+    
+#     if(0<=nx<n and 0<=ny<m and box[nx][ny]==0):
+#       box[nx][ny]=box[x][y]+1 #day 하루 빼야 함.
+#       queue.append((nx,ny))
+
+# result=0
+# for i in box:
+#   if 0 in i:
+#     print(-1) # 모두 익지 않은 경우
+#     exit()
+#   result= max(max(i),result)
+
+# print(result-1)
+
+
 from collections import deque
 m,n= map(int,input().split())
 box=[]
@@ -29,9 +64,9 @@ ripen(rippend)
 result=0
 for i in box:
   if 0 in i:
-    result=-1 # 모두 익지 않은 경우
-    break
-  result= max(max(i)-1,result)
+    print(-1)
+    exit()
+  result= max(max(i),result)
 
-print(result)
+print(result-1)
 
