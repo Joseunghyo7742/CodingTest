25d6c0ddaa8d402159c4ee14be58260c05fb0716
Jo Seung Hyo <seunghyo7742@naver.com>
2023-07-30 19:39:36 +0900

    Folder moved

--- ProgrammersKit/Sort_HIndex_lv2.py
+++
@@ -1,7 +0,0 @@
-def solution(citations):
-    citations = sorted(citations)
-    l = len(citations)
-    for i in range(l):
-        if citations[i] >= l-i:
-            return l-i #l-i가 h임 즉 h개 이상인지 확인하는 작업.
-    return 0
