4bbbab8fd6ce290b10272f6c5520fbaa7b90592e
Jo Seung Hyo <seunghyo7742@naver.com>
2023-10-11 16:15:02 +0900

    DFS: 연결요소의 개수 복습

---
+++ PythonCodingTest/백준/복습/연결요소의개수.py
@@ -0,0 +1,24 @@
+#정점의 개수 N, 간선의 개수 M
+n,m = map(int,input().split())
+arr= [[0]*n for _ in range(n)]
+
+for i in range(m):
+  u,v= map(int,input().split())
+  arr[u-1][v-1]=1
+  arr[v-1][u-1]=1
+  
+
+check=[False]*n;
+count=0
+
+def dfs(i):
+  for j in range(n):
+    if arr[i][j]==1 and not check[j]:
+      check[j]=True
+      dfs(j)
+for i in range(n):
+  if not check[i]:
+    count+=1
+    count[i]=True
+    dfs(i)
+print(count)
