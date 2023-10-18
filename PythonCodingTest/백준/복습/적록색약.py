620c06035c66e3ebfdf3f9be849a1fc17512b0ee
Jo Seung Hyo <seunghyo7742@naver.com>
2023-10-18 09:27:50 +0900

    적록색약복습

--- PythonCodingTest/백준/복습/적록색약.py
+++ PythonCodingTest/백준/복습/적록색약.py
@@ -10,39 +10,42 @@ for _ in range(n):
 
 check=[[0]*n for _ in range(n)]
 d= [(0,-1),(0,1),(1,0),(-1,0)]
-switch= 1 
+
+#dfs 제사용을 위해 방문 여부를 visited로 체크한다.
+visited= 1 
 
 def dfs(c,r,color):
   global check
-  if check[c][r] == switch:
+  if check[c][r] == visited: #방문여부 확인 
     return
-  check[c][r]=switch
-  for dc, dr in d:
+  check[c][r]=visited #방문 체크
+  for dc, dr in d: #4방향 dfs탐색 
     new_c = c+ dc
     new_r= r+dr
-    if(new_c<0 or new_c>=n or new_r<0 or new_r>=n):
+    if(new_c<0 or new_c>=n or new_r<0 or new_r>=n):#범위가 같지않다면 스킵
       continue
-    if(palette[new_c][new_r] not in color):
+    if(palette[new_c][new_r] not in color): #색상이 같지않다면 스킵
       continue
-    dfs(new_c,new_r,color)
+    dfs(new_c,new_r,color) 
   
 
 
-result1=0
-result2=0
-#적록색약 x의 경우
+result1=0 #적록색약 아닌 경우
+result2=0 #적록색약인 경우
+
+#적록색약 아닌 경우
 for i in range(n):
   for j in range(n):
-    if(check[i][j]!=switch):
+    if(check[i][j]!=visited):
       dfs(i,j,palette[i][j])
       result1+=1
 
-
-switch=0
+#적록색약인 경우, 방문한 곳을 0으로 본다.
+visited=0 
 #적록색약인 경우 
 for i in range(n):
   for j in range(n):
-    if(check[i][j]!=switch):
+    if(check[i][j]!=visited):
       if palette[i][j]=="R" or palette[i][j]=="G":
         dfs(i,j,"RG")
       else:
