4395208717b7669c36417add33c0c24e17c6e046
Jo Seung Hyo <seunghyo7742@naver.com>
2024-01-29 15:45:42 +0900

    백준 파이썬 AtoB

---
+++ PythonCodingTest/백준/그리디/AtoB.py
@@ -0,0 +1,14 @@
+a,b= map(int,input().split())
+
+result=1
+while(a!=b and b>a):
+  if(b%2==0):
+      b=b//2
+      result+=1
+  elif(b%10==1):
+    b= (b-1)//10
+    result+=1
+  else: break
+  
+if(b!=a): result=-1
+print(result)
