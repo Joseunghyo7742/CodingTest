7e87c68b220f161f2e3ee244a3eea750af6d5107
Jo Seung Hyo <seunghyo7742@naver.com>
2024-05-06 23:20:03 +0900

    골드바흐의 추측

--- 백준/수학,브루트포스/골드바흐의추측.js
+++ 백준/수학,브루트포스/골드바흐의추측.js
@@ -13,27 +13,28 @@ const error_message = "Goldbach's conjecture is wrong.";
 const arr = Array.from({ length: 1000001 }, () => 1); //길이가 1000000인 0인 배열 생성
 for (let i = 3; i <= 499999; i += 2) {
   if (arr[i]) {
-    console.log('i', i);
-
     for (let j = 3; i * j <= 1000001; j += 2) {
       arr[i * j] = 0;
     }
   }
 }
 
+const answer = [];
 let idx = 0;
 while (input[idx] !== 0) {
   const num = input[idx];
   let check = false; //해당 짝수가 두 소수의 합으로 이뤄지는게 있는지 확인.
-  for (let i = num - 3; i >= num / 2; i -= 2) {
+  for (let i = 3; i <= num / 2; i += 2) {
     if (arr[i] && arr[num - i]) {
-      console.log(`${num} = ${num - i} + ${i}`);
+      answer.push(`${num} = ${i} + ${num - i}`);
       check = true;
       break;
     }
   }
   idx += 1;
   if (!check) {
-    console.log(error_message);
+    answer.push(error_message);
   }
 }
+
+console.log(answer.join('\n'));
