010d99f5e749c5e126520e289b2399bfb80bcd8c
Jo Seung Hyo <seunghyo7742@naver.com>
2024-05-06 19:02:45 +0900

    failed 골드바흐의추측

--- 백준/수학,브루트포스/골드바흐의추측.js
+++ 백준/수학,브루트포스/골드바흐의추측.js
@@ -9,33 +9,24 @@ const input = fs
   .map(Number);
 const error_message = "Goldbach's conjecture is wrong.";
 
-//소수 판별 함수
-function isDecimal(num) {
-  //제곱근보다 낮은 홀수로만 나눠보기
-  for (let i = 3; i < parseInt(Math.sqrt(num)); i += 2) {
-    if (num % i === 0) return false;
+//소수면 false
+const arr = Array.from({ length: 1000001 }, () => 1); //길이가 1000000인 0인 배열 생성
+for (let i = 3; i <= 499999; i += 2) {
+  if (arr[i]) {
+    console.log('i', i);
+
+    for (let j = 3; i * j <= 1000001; j += 2) {
+      arr[i * j] = 0;
+    }
   }
-  return true;
 }
 
-// for (const num of input) {
-//   if (num === 0) break;
-//   else {
-//     //절반 이하의 홀수는 순서만 바뀌는 거기 때문에 보지않음
-//     for (let i = num - 3; i >= num / 2; i -= 2) {
-//       if (isDecimal(i) && isDecimal(num - i)) {
-//         console.log(`${num} = ${num - i} + ${i}`);
-//         break;
-//       }
-//     }
-//   }
-// }
 let idx = 0;
 while (input[idx] !== 0) {
   const num = input[idx];
-  let check = false;
+  let check = false; //해당 짝수가 두 소수의 합으로 이뤄지는게 있는지 확인.
   for (let i = num - 3; i >= num / 2; i -= 2) {
-    if (isDecimal(i) && isDecimal(num - i)) {
+    if (arr[i] && arr[num - i]) {
       console.log(`${num} = ${num - i} + ${i}`);
       check = true;
       break;
