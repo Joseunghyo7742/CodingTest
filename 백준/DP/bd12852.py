6971a22f19915abbd11614e20e3440c8c4830f80
seunghyo <seunghyo7742@naver.com>
2024-10-17 17:24:03 +0900

    백준: 12852

---
+++ 백준/DP/bd12852.py
@@ -0,0 +1,24 @@
+# 1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
+# 2. X가 2로 나누어 떨어지면, 2로 나눈다.
+# 3. 1을 뺀다.
+
+# 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
+
+
+n= int(input())
+track=[[1]]
+
+for i in n:
+  temp=[i]
+  if(i%3 ==0):
+    #d[k]= d[k//3]+1
+    temp.extend(track[i//3])
+  elif(i%2==0):
+    #d[k]= d[k//2]+1
+    temp.extend(track[i//2])
+  else:
+    temp.extend(track[i-1])
+  track.append(temp)
+
+console.log(track)
+  
