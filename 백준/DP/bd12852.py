3e7348c6238018fa549c8f3e47a8387aefecfee8
Jo Seung Hyo <seunghyo7742@naver.com>
2024-10-18 00:04:40 +0900

    a

--- 백준/DP/bd12852.py
+++ 백준/DP/bd12852.py
@@ -10,14 +10,35 @@ track=[[0],[1]]
 
 for i in range(2,n+1):
   temp=[i]
-  if(i%3 ==0):
-    #d[k]= d[k//3]+1
-    temp.extend(track[i//3])
-  elif(i%2==0):
-    #d[k]= d[k//2]+1
-    temp.extend(track[i//2])
-  else:
-    temp.extend(track[i-1])
+  prev= track[i-1]
+  if(i%2==0):
+    a= track[i//2]
+    if(len(prev)>len(a)):
+      temp.extend(a)
+    else:
+      temp.extend(prev)
+  elif(i%3==0):
+    b= track[i//3]
+    if(len(prev)>len(b)):
+      temp.extend(b)
+    else:
+      temp.extend(prev)
+  track.append(temp)
+  
+  # if(i%3 ==0):
+  #   #d[k]= d[k//3]+1
+  #   temp.extend(track[i//3])
+  # elif(i%2==0):
+  #   #d[k]= d[k//2]+1
+  #   a= track[i//2]
+  #   b= track[i-1]
+  #   if(len(b)<len(a)):
+  #     temp.extend(b)
+  #   else:
+  #     temp.extend(track[i//2])
+  # else:
+  #   temp.extend(track[i-1])
   track.append(temp)
 
-print(track)  
+print(len(track[n])-1)
+print(*track[n])
