4e8874f2b4114eef62754503ba3bb106f975e261
Jo Seung Hyo <seunghyo7742@naver.com>
2025-01-08 00:36:06 +0900

    1-5 모이기

--- 강의/1-5_모이기.py
+++ 강의/1-5_모이기.py
@@ -6,32 +6,72 @@
 # X,y를 구한다음 거리를 구한다 
 # 각 경우의 수에서 최소 값을 구한다
 
-n= int(input())
-d=[ list(map(int,input().split())) for _ in range(n)]
-answer=[-1]*n
+# n= int(input())
+# d=[ list(map(int,input().split())) for _ in range(n)]
+# answer=[-1]*n
 
-for x in range (1,1_001):
-  for y in range (1,1_001):
-    distance=[] 
-    for item in d:
-      a,b=item # 위치
-      distance.append(abs(a-x)+abs(b-y))
+# for x in range (1,1_001):
+#   for y in range (1,1_001):
+#     distance=[] 
+#     for item in d:
+#       a,b=item # 위치
+#       distance.append(abs(a-x)+abs(b-y))
 
-    # 가까운 순 배치
-    distance.sort()
+#     # 가까운 순 배치
+#     distance.sort()
     
-    tmp=0
-    # 한명, 두명, 세명, n명 모일때 최소값을 더해주어야 함.
-    for i in range(len(distance)):
-      di= distance[i]
-      tmp+=di
-      if(answer[i]==-1):
-        answer[i]=tmp
-      else:
-        answer[i]= min(tmp, answer[i])
+#     tmp=0
+#     # 한명, 두명, 세명, n명 모일때 최소값을 더해주어야 함.
+#     for i in range(len(distance)):
+#       di= distance[i]
+#       tmp+=di
+#       if(answer[i]==-1):
+#         answer[i]=tmp
+#       else:
+#         answer[i]= min(tmp, answer[i])
         
 
-print(*answer)
+# print(*answer)
     
     
+
+# 2번 아이디어 
+# 우리의 집 중 한 곳에서 모인다.
+# 그 점에서 가까운 사람만 더해주자
+# 각 집에서 다른 집까지의 거리들을 배열로 두기
+# 오름차순 정렬 후, n명 모였을 때 각각 배열에서 n번째합의 최소값
+
+n= int(input())
+# home=[list(map(int,input().split())) for _ in range(n)]
+
+x=[]
+y=[]
+arr=[]
+
+answer=[-1]*n
+for _ in range(n):
+  a,b= map(int,input().split())
+  x.append(a)
+  y.append(b)
+  arr.append([a,b])
+  
+  
+for i in x:
+  for j in y:
+    dist=[]
+    
+    for a,b in arr:
+      d= abs(i-a)+abs(j-b)
+      dist.append(d)
       
+    dist.sort()
+    
+    tmp=0
+    for i in range(len(dist)):
+      d= dist[i]
+      tmp+=d
+      if answer[i]==-1:
+        answer[i]= tmp
+      else:
+        answer[i]= min(tmp, answer[i])
+
